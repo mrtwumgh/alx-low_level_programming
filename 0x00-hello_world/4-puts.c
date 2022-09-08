9587934128624df0ee35d3afbd561905e9e5bca2
mrtwumgh <twumstephen2006@gmail.com>
2022-09-08 11:22:55 +0000

    4-puts.c program added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * A C program that prints a string using the Puts Function
+ * Displays the string "Programming is like building a multilingual puzzle"
+ *
+ */
+int main(void)
+{
+	char str[] = "Programming is like building a multilingual puzzle";
+	puts(str);
+	return (0);
+}
