e957602fc0b6f5ed757c0a8968ac74fb0724fad7
mrtwumgh <twumstephen2006@gmail.com>
2022-09-08 11:37:05 +0000

    5-printf.c program added

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A C program that prints using printf
+ * Return: Program should return 0
+ * Program should compile without warnings when using the -wall gcc option
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
