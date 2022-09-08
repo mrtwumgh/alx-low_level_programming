d1709d949e3dd3b8280e64c67ad653c63e33dcfe
mrtwumgh <twumstephen2006@gmail.com>
2022-09-08 14:23:16 +0000

    101-quote.c program added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - A C Program that prints a string
+ * main -  A C program that writes to a new line and standard error
  * Return: Program should return 1
- * Should compile without any warnings
  *
  */
 int main(void)
 {
-	write("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr)
 	return (1);
 }
