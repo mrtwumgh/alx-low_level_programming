2dc1713721d69eab0dd2e921e59552c6e9534d12
mrtwumgh <twumstephen2006@gmail.com>
2022-09-08 12:43:30 +0000

    101-quote.c program added

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A C Program that prints a string
+ * Return: Program should return 1
+ * Should compile without any warnings
+ *
+ */
+int main(void)
+{
+	write("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
