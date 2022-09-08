92b6d0818725adbc33be239f04e2bf77e58f7767
mrtwumgh <twumstephen2006@gmail.com>
2022-09-08 11:27:43 +0000

    4-puts.c program fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 /**
- * A C program that prints a string using the Puts Function
- * Displays the string "Programming is like building a multilingual puzzle"
+ * main - A C program that prints a string using the Puts Function
+ * Displays the string "Programming is like building a multilingual puzzle
+ * Return: Program should end with the value 0
  *
  */
+
 int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle";
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
 	return (0);
 }
