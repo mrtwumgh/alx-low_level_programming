e4fc32ebe46bb6122f999c2589fa7628fbcba99f
mrtwumgh <twumstephen2006@gmail.com>
2022-09-09 15:14:06 +0000

    print_numbers.c program added

---
+++ 0x01-variables_if_else_while/print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - Entry point for code
+*Description - prints out all numbers in base 10
+*Return: should return a value of 0
+*
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
