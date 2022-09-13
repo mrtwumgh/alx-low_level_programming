d5979f1418410aeb4f45047ba5b7a936edae3989
mrtwumgh <twumstephen2006@gmail.com>
2022-09-13 11:36:02 +0000

    1-alphabet.c program added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*main - entry point to code
+*Description - Prints the alphabet, in lower case
+*Return: returns a value of 0
+*
+*/
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+		_putchar('\n');
+	return (0);
+}
