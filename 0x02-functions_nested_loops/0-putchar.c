4f0beb327c28f89ff6a00b0fe60a866e18ef713d
mrtwumgh <twumstephen2006@gmail.com>
2022-09-13 09:57:30 +0000

    0-putchar.c program added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - entry point to code
+*Description - A program that prints _putchar
+*Return: should return a 0 value
+*
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
