28e5d30f3ba95fe6c52ee54bbd0dba062eaea812
mrtwumgh <twumstephen2006@gmail.com>
2022-09-13 22:21:45 +0000

    8-24_hours program added

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*jack_bauer - prints every minute in 24 hours
+*Return: does not return a value
+*
+*/
+
+void jack_bauer(void)
+{
+	int j;
+	int b;
+
+	for (j = 0; j < 24; j++)
+	{
+		for (b = 0; b < 60; b++)
+		{
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
