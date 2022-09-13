87ba70216c660b24764dd4b13abaee7f183c1d5a
mrtwumgh <twumstephen2006@gmail.com>
2022-09-13 12:15:48 +0000

    fix for 1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -12,7 +12,9 @@ int main(void)
 	char a;
 
 	for (a = 'a'; a <= 'z'; a++)
+	{
 		_putchar(a);
-		_putchar('\n');
+	}
+	_putchar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*main - entry point to code
+*Description - checks for a lowercase character
+*Return: should return a value of 1 if true else 0
+*
+*/
+
+int main(void)
+{
+	char c;
+
+	if (_islower(c))
+	{
+		_putchar(
