edf4c53c967e4355433282f916282a7309bb173f
mrtwumgh <twumstephen2006@gmail.com>
2022-09-13 23:19:28 +0000

    102-fibonacci program added

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+*main - entry point to the code
+*Description - prints the first 50 fibonacci numbers
+*Return: a value of 0
+*
+*/
+
+int main(void)
+{
+	long int a;
+	long int b;
+
+	a = 1;
+	B = 2;
+	for (c = 1; c <= 50; ++c)
+	{
+		if (a != 20365011074)
+		{
+			printf("%ld, ", a);
+		} else
+		{
+			printf("%ld\n", a);
+		}
+		next = a + b;
+		a = b;
+		b = next;
+	}
+	return (0);
+}
