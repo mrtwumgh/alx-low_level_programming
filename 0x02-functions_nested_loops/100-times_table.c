47857d865d8ce015ffcba73ed85b3a75fc5b4d65
mrtwumgh <twumstephen2006@gmail.com>
2022-09-14 18:47:26 +0000

    100-times _table.c program added

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,4 @@
 #include "main.h"
-i
 /**
 *print_times_table - prints the the 100 times table
 *@n: number to be processed
@@ -8,25 +7,32 @@ void print_times_table(int n)
 {
 	int m, o, p;
 
-	if (n >= 0 && n <= 15){
-		for (m = 0; m <= n; m++){
-			for (o = 0; o <= n; o++){
+	if (n >= 0 && n <= 15)
+	{
+		for (m = 0; m <= n; m++)
+		{
+			for (o = 0; o <= n; o++)
+			{
 				p = o + m;
-				if (o == 0){
+				if (o == 0)
+				{
 					_putchar(p + '0');
-				}else if (p < 10 && != 0){
+				} else if (p < 10 && != 0)
+				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(p + '0');
-				}else if (p >= 10 && p < 100){
+				} else if (p >= 10 && p < 100)
+				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((p / 10) + '0');
 					_putchar((p % 10) + '0');
-				}else if (p >= 100){
+				} else if (p >= 100)
+				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar((p / 100) + '0');
