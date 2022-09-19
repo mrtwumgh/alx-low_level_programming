7625cddf03423ec71683a93c98cc6765aab5470c
mrtwumgh <twumstephen2006@gmail.com>
2022-09-19 21:37:36 +0000

    2-strlen.c added

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  *_strlen - returns the length of a string
+  *@s: the string to be processed
+  *Return: the string length
+  */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+	return (len);
+}
