5aaf89b8d60334e4e2e224304f4621adceb335d2
mrtwumgh <twumstephen2006@gmail.com>
2022-09-22 22:05:27 +0000

    5-string_toupper.c added

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * string_toupper - changes lowercase to uppercase
+  *@s: string
+  *Return: the resulting string
+  */
+
+char 8string_toupper(char *s)
+{
+	int a;
+
+	a = 0;
+
+	while (*(s + a))
+	{
+		if ((*(s + a) >= 97) && (*(s + a) <= 122))
+			*(s + a) = *(s + a) - 32;
+		a++;
+	}
+	return (s);
+}
