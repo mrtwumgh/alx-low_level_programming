1cda59477785e6e6f310ca8c53c3d6001a6c4dfd
mrtwumgh <twumstephen2006@gmail.com>
2022-09-23 21:14:59 +0000

    100-rot13.c added

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * rot13 - encodes a string using rot13
+  *@s: string
+  *Return: Return string
+  */
+
+char *rot13(char *s)
+{
+	int x;
+
+	x = 0;
+
+	while (s[x])
+	{
+		while ((s[x] >= 'a' && s[x] <= 'z') || (s[x] >= 'A' && s[x] <= 'Z'))
+		{
+			if ((s[x] > 'm' && s[x] <= 'z') || (s[x] > 'M' && s[x] <= 'Z'))
+			{
+				s[x] -= 13;
+				break;
+			}
+			s[x] += 13;
+			break;
+		}
+		x++
+	}
+	return (s);
+}
