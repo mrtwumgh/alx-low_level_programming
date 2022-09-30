7d39f59ea69ade018e6f436be97abd99b86c0549
mrtwumgh <twumstephen2006@gmail.com>
2022-09-30 21:00:00 +0000

    3-mul.c added

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+  * main - entry point into code
+  *@argc: counter for arguments
+  *@argv: vector for strings to arguments to be passed
+  *Return: 0
+  *
+  */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	(void)argc;
+	(void)argv;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	printf("%d\n", a * b);
+
+	return (0);
+}
