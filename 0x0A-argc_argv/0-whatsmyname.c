532aa839639a9f8b7bca54ec42ec89261ec998ea
mrtwumgh <twumstephen2006@gmail.com>
2022-09-30 20:44:44 +0000

    0-whatsmyname.c added

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point to the code
+  *@argc: counter for arguments
+  *@argv: vector of strings with arguments to be passed
+  *Return: 0
+  *
+  */
+
+int main(int argc, char *argv[1])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
