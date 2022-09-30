bb96fbf3f4d79a2a8d316647fae9e5c04cfbc0be
mrtwumgh <twumstephen2006@gmail.com>
2022-09-30 21:11:09 +0000

    4-add.c added

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+  * is_num - checks if string is numeric
+  *@s: string to be processed
+  *Return: True if numeric else false
+  *
+  */
+
+bool is_num(char *s)
+{
+	int i;
+
+	for (i = 0l s[i] != '\0'; i++)
+	{
+		if (s[i] < 48 || s[i] > 57)
+		{
+			return (false);
+		}
+	}
+	return (true);
+}
+
+/**
+  * main - entry point into code
+  *@argc: counter for arguments
+  *@argv: argument array
+  *Return: 0
+  *
+  */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+	(void)argc;
+	(void)argv;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!is_num(argv[i]))
+		{
+			print("Error\n")
+			return (1);
+		}
+		sum = sum + atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
