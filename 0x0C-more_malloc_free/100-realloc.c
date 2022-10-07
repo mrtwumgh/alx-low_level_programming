a4c5aa8abae6acd8271c0db7f5419b7f33e625ab
mrtwumgh <twumstephen2006@gmail.com>
2022-10-07 21:14:26 +0000

    100-realloc.c added

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -36,7 +36,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size < old_size)
 	{
-		for (i = ol i < new_size; i++)
+		for (i = 0; i < new_size; i++)
 			p[i] = op[i];
 	}
 
