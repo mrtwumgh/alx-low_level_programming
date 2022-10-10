6195ea7b82bca175c9528c00a25b4c5b922c78d9
mrtwumgh <twumstephen2006@gmail.com>
2022-10-10 20:35:34 +0000

    Dog.h header added

---
+++ 0x0E-structures_typdef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - defined data type dog
+  * @name: defined element name
+  * @age: defined element age
+  * @owner: defined element owner
+  *
+  * Description: various elements that can be used to define a dog
+  *
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
