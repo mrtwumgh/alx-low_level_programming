77a2b11d15dc16076bf048d7a7b992fb8c14653c
mrtwumgh <twumstephen2006@gmail.com>
2022-10-10 22:47:52 +0000

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef OBJECT_LIKE_MACR0_H
-#define OBJECT_LIKE_MACRO_H
+#ifndef _OBJECT_LIKE_MACRO_H_
+#define _OBJECT_LIKE_MACRO_H_
 
 #define SIZE 1024
 
-#endif /* OBJECT_LIKE_MACRO_H */
+#endif /* _OBJECT_LIKE_MACRO_H_ */
