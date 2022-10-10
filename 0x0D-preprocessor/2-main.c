22f02e0a2fcf280a1e79986efcdaf26492940543
mrtwumgh <twumstephen2006@gmail.com>
2022-10-10 22:53:38 +0000

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - main entry into the code
+  *
+  *Description: a function that prints the name of file compiled from
+  *Return: returns a value of 0
+  *
+  */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
