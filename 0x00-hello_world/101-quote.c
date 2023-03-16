d0e16c625ed3cd16eefae7d2429f4d8a3962be8d
mrtwumgh <twumstephen2006@gmail.com>
2023-03-16 08:06:24 +0000

    101-quote.c program added

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+  * main - Entry Point
+  *
+  * Return: 1
+  */
+
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, quote, strlen(quote));
+
+	return (1);
+}
