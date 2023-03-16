431d20b282a1f6910048c9c369ec2d5f36498372
mrtwumgh <twumstephen2006@gmail.com>
2023-03-16 07:49:02 +0000

    6-size.c program added

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - Entry Point
+  *
+  * Return: Always 0(Success)
+  */
+
+int main(void)
+{
+	printf("Size of a char: %zd byte(s)\n", sizeof(char));
+	printf("Size of an int: %zd byte(s)\n", sizeof(int));
+	printf("Size of a long long int: %zd byte(s)\n", sizeof(long long));
+	printf("Size of a float: %zd byte(s)\n", sizeof(float));
+
+	return (0);
+}
