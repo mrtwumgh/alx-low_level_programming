93c25f8f88f43e84f4515afdb961bda8d28fe938
mrtwumgh <twumstephen2006@gmail.com>
2023-03-16 07:50:22 +0000

    6-size.c program added

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,7 @@ int main(void)
 {
 	printf("Size of a char: %zd byte(s)\n", sizeof(char));
 	printf("Size of an int: %zd byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zd bytes(s)\n", sizeof(long));
 	printf("Size of a long long int: %zd byte(s)\n", sizeof(long long));
 	printf("Size of a float: %zd byte(s)\n", sizeof(float));
 
