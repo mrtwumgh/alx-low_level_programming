879b3e7018fd6d392b01dd43151b0e70c69eacd7
mrtwumgh <twumstephen2006@gmail.com>
2023-03-16 08:07:50 +0000

    101-quote.c program added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,9 @@
 
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, quote, strlen(quote));
+	write(2, str, strlen(str));
 
 	return (1);
 }
