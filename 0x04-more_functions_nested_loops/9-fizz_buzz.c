1f7d5310669923de701cf9899799588924f0d485
mrtwumgh <twumstephen2006@gmail.com>
2023-03-23 12:07:57 +0000

    9-fizz_buzz.c fix added

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,17 +12,17 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if (x % 3 == 0)
+		if (x % 3 == 0 && x % 5 == 0)
 		{
-			printf("%s ", "Fizz");
+			printf("%s ", "FizzBuzz");
 		}
 		else if (x % 5 == 0)
 		{
 			printf("%s ", "Buzz");
 		}
-		else if (x % 3 == 0 && x % 5 == 0)
+		else if (x % 3 == 0)
 		{
-			printf("%s ", "FizzBuzz");
+			printf("%s ", "Fizz");
 		}
 		else
 		{
