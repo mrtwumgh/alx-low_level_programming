9dff88a64c948fe60c64f28894b3019af2dcf9df
mrtwumgh <twumstephen2006@gmail.com>
2023-03-23 07:46:18 +0000

    9-fizz_buzz.c program added

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+  * main - The Fizz-Buzz Test
+  *
+  * Return: Always 0(Success)
+  */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0)
+		{
+			printf("%s ", "Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("%s ", "Buzz");
+		}
+		else if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("%s ", "FizzBuzz");
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
