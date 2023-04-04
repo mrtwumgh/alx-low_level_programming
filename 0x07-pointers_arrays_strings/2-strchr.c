77597ea954c40e0c77b74d8a24f91b330a28e698
mrtwumgh <twumstephen2006@gmail.com>
2023-04-04 08:42:49 +0000

    2-strchr.c program added

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+  * _strchr - locates a character in a string
+  * @s: string to be processed
+  * @c: the character to be located
+  *
+  * Return: the pointer to the character located
+  */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	char *p = NULL;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			p = &s[i];
+			break;
+		}
+		i++;
+	}
+
+	return (p);
+}
