dc4a3529be25274ed16b1b1300bcdc0bcd553a94
mrtwumgh <twumstephen2006@gmail.com>
2023-04-07 08:40:09 +0000

    0-whatsmyname.c program added

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * main - entry point, prints out the program name
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always 0(Success)
+  */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
