3d0c15716c698d3059aacd033a1c14d8791095e1
mrtwumgh <twumstephen2006@gmail.com>
2023-04-13 12:08:48 +0000

    1-string_nconcat fix added

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -62,7 +62,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		st[i] = s1[i];
 	}
 
-	for (i = 0; i < n && s2[i] != '\0'; i++)
+	for (i = 0; i < n; i++)
 	{
 		st[len1 + i] = s2[i];
 	}
