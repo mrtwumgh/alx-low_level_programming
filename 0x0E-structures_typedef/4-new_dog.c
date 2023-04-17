7cf559268721c4420db7bcea701cdeef562de1c7
mrtwumgh <twumstephen2006@gmail.com>
2023-04-17 12:52:11 +0000

    4-new_dog.c fix added

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,66 @@
 #include "dog.h"
 
 /**
+  * _strlen - gets the length of a string
+  * @s: string to be processed
+  *
+  * Return: The length of the string
+  */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	if (s == NULL)
+	{
+		return (len);
+	}
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
+
+/**
+  * _strdup - creates a copy of a string
+  * @s: string to be processed
+  *
+  * Return: pointer to the copy of the string
+  */
+
+char *_strdup(char *s)
+{
+	char *ps;
+	int slen = 0;
+	int i = 0;
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	slen = _strlen(s);
+
+	ps = (char *)malloc(sizeof(*ps) * (slen + 1));
+
+	if (ps == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		ps[i] = s[i];
+	}
+	ps[i] = '\0';
+
+	return (ps);
+}
+
+/**
   * new_dog - creates a new dog
   * @name: name of dog
   * @age: age of dog
@@ -12,16 +72,33 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *pd;
+	char *tempname;
+	char *tempowner;
 
 	pd = malloc(sizeof(dog_t));
+
 	if (pd == NULL)
 	{
 		return (NULL);
 	}
 
-	pd->name = name;
+	tempname = _strdup(name);
+	if (tempname == NULL)
+	{
+		free(pd);
+		return (NULL);
+	}
+	pd->name = tempname;
 	pd->age = age;
-	pd->owner = owner;
+
+	tempowner = _strdup(owner);
+	if (tempowner == NULL)
+	{
+		free(pd->name);
+		free(pd);
+		return (NULL);
+	}
+	pd->owner = tempowner;
 
 	return (pd);
 }
