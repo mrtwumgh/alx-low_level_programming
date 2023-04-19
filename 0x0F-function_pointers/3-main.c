f160014e8c65bff7929c5ea31874a0684f755a64
mrtwumgh <twumstephen2006@gmail.com>
2023-04-19 22:35:05 +0000

    all functions added

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+
+/**
+  * main - entry point into code
+  * @argc: argument counter
+  * @argv: argument vector
+  *
+  * Return: Always 0(Success)
+  */
+
+int main(int argc, char *argv[])
+{
+	int result;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2] == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	func = get_op_func(argv[2]);
+
+	if ((argv[2][0] == '/' && argv[3] == 0) ||
+	    (argv[2][0] == '%' && argv[3] == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = func(atoi(argv[1]), atoi(argv[3]));
+
+	printf("%d\n", result);
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+  * op_add - returns the sum of 2 integers
+  * @a: first integer
+  * @b: second integer
+  *
+  * Return: The sum
+  */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * op_sub - returns the difference of 2 integers
+  * @a: first integer
+  * @b: second integer
+  *
+  * Return: The difference
+  */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * op_mul - returns the product of 2 integers
+  * @a: first integer
+  * @b: second integer
+  *
+  * Return: The product
+  */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * op_div - returns the quotient of 2 integers
+  * @a: first integer
+  * @b: second integer
+  *
+  * Return: The quotient
+  */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  * op_mod - returns the remainder of 2 integers
+  * @a: first integer
+  * @b: second integer
+  *
+  * Return: The remainder
+  */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
