fc38f7fef6dcae73142b4e1d125d4a2a73db67ae
mrtwumgh <twumstephen2006@gmail.com>
2023-04-19 13:26:53 +0000

    0-print_name.c program added

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -10,5 +10,9 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL)
+	{
+		return;
+	}
 	(*f)(name);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTION_POINTERS_H_
 #define FUNCTION_POINTERS_H_
 
-int _putchar(char c);
+#include <stdio.h>
 
 void print_name(char *name, void (*f)(char *));
 
