83b2e68427245b4af9446ef84f6c1b7e62ee3a0d
mrtwumgh <twumstephen2006@gmail.com>
2023-04-20 20:09:20 +0000

    1-print_numbers.c program added

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+
+/**
+  * print_numbers - prints numbers, followed by a new line
+  * @separator: separates numbers
+  * @n: control variable
+  *
+  * Return: Void
+  */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+	unsigned int num;
+
+	if (separator == NULL || n == 0)
+	{
+		return;
+	}
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(list, int);
+		printf("%d", num);
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	putchar('\n');
+
+	va_end(list);
+
+}
