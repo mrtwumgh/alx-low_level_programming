699cb2bd98172ef1fe70b5fd0ed4ec05cd806b65
mrtwumgh <twumstephen2006@gmail.com>
2023-04-27 20:52:07 +0000

    0-print_list.c added

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+  * print_list - prints a list
+  * @h: head node
+  *
+  * Return: number of nodes
+  */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+	const list_t *current;
+
+	current = h;
+
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		printf("[%u] %s\n", current->len, current->str);
+		current = current->next;
+		i++;
+	}
+
+	return (i);
+}
