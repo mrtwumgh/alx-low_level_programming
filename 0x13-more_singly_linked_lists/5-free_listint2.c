f4a7e7e35f9f5247a7cc9960cc33d5e689ad8c43
mrtwumgh <twumstephen2006@gmail.com>
2023-05-01 09:03:04 +0000

    5-free_listint2.c program added

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+  * free_listint2 - frees a listint_t list
+  * @head: head node
+  *
+  * Return: Void
+  */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (*head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
+
+	*head = NULL;
+}
