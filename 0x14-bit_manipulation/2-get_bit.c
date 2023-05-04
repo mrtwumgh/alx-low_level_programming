1b22ce2ac544aaa7ee27c1beeb77ebf3c44bfdb6
mrtwumgh <twumstephen2006@gmail.com>
2023-05-04 11:16:30 +0000

    2-get_bit.c addition to code added

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,6 +13,11 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned int mask = 1 << index;
 	int result = 0;
 
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
 	if (n && mask)
 	{
 		result = (n & mask) ? 1 : 0;
