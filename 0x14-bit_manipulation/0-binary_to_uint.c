d5f1ff71d787fdb059407379d5daa5243fc73a98
mrtwumgh <twumstephen2006@gmail.com>
2023-05-04 12:22:39 +0000

    0-binary_to_uint.c fix added

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -40,14 +40,11 @@ unsigned int binary_to_uint(const char *b)
 	}
 	for (i = 0; i < len; i++)
 	{
-		if (b[i] == '1' || b[i] == '0')
-		{
-			result = 2 * result + (b[i] - '0');
-		}
-		else
+		if (b[i] < '0' || b[i] > '1')
 		{
 			return (0);
 		}
+		result = 2 * result + (b[i] - '0');
 	}
 
 	return (result);
