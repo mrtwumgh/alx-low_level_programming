98cca031c397a2aa8f9eefcf1d71565da43f2924
mrtwumgh <twumstephen2006@gmail.com>
2023-05-04 12:23:50 +0000

    0-binary_to_uint.c added

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,6 @@
 #include "main.h"
 
 /**
-  * _strlen - returns the length of a string
-  * @s: the string
-  *
-  * Return: the length of the string
-  */
-
-int _strlen(const char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-
-	return (i);
-}
-
-/**
   * binary_to_uint - converts binary number to an int
   * @b: binary notation
   *
@@ -30,15 +11,12 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
 	int i = 0;
-	int len = 0;
-
-	len = _strlen(b);
 
 	if (b == NULL || *b == '\0')
 	{
 		return (0);
 	}
-	for (i = 0; i < len; i++)
+	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
 		{
