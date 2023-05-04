5f0231d318290ca660c5a448e653ece72d1c4f7f
mrtwumgh <twumstephen2006@gmail.com>
2023-05-04 10:30:28 +0000

    0-binary_to_uint.c added

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+  * _strlen - returns the length of a string
+  * @s: the string
+  *
+  * Return: the length of the string
+  */
+
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+  * binary_to_uint - converts binary number to an int
+  * @b: binary notation
+  *
+  * Return: the decimal notation
+  */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int result = 0;
+	int i = 0;
+	int len = 0;
+
+	len = _strlen(b);
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	for (i = 0; i < len; i++)
+	{
+		if (b[i] == '1' || b[i] == '0')
+		{
+			result <<= 1;
+			if (b[i] == '1')
+			{
+				result |= 1;
+			}
+		}
+		else
+		{
+			return (0);
+		}
+	}
+
+	return (result);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdlib.h>
+
+unsigned int binary_to_uint(const char *b);
+
+
+#endif /* MAIN_H_ */
