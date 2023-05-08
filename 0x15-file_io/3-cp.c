3385d953b4ab253334bcfb7f148c03f529e5db3c
mrtwumgh <twumstephen2006@gmail.com>
2023-05-08 13:28:29 +0000

    3-cp.c fix added

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,12 +13,12 @@ void print_error(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Can't read from the file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from the file %s\n", argv[1]);
 		exit(98);
 	}
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Can't write to the %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 }
