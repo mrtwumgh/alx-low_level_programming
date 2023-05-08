7b37957e08e8c4396c99ecc3d1df836472282d7a
mrtwumgh <twumstephen2006@gmail.com>
2023-05-08 11:38:44 +0000

    0-read_textfile.c additions

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,32 +12,40 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t bytes_read;
+	ssize_t bytes_written;
 	char *pbuffer;
 
 	if (filename == NULL)
 	{
 		return (0);
 	}
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		return (0);
 	}
-
 	pbuffer = (char *)malloc(sizeof(char) * (letters + 1));
 	if (pbuffer == NULL)
 	{
 		close(fd);
 		return (0);
 	}
-
 	bytes_read = read(fd, pbuffer, letters);
+	if (bytes_read == -1)
+	{
+		free(pbuffer);
+		close(fd);
 
-	write(STDOUT_FILENO, pbuffer, letters);
-
+		return (0);
+	}
+	bytes_written = write(STDOUT_FILENO, pbuffer, bytes_read);
+	if (bytes_written == -1)
+	{
+		free(pbuffer);
+		close(fd);
+		return (0);
+	}
 	free(pbuffer);
 	close(fd);
-
 	return (bytes_read);
 }
