a3343193bc168d4cb6338ac73efa3749ad354238
mrtwumgh <twumstephen2006@gmail.com>
2023-10-22 21:16:58 +0000

    7-insert_dnodeint.c fix added

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,36 +12,32 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *newnode = NULL, *temp = NULL;
-	unsigned int count = 0, i = 0;
+	unsigned int count = 0;
 
+	if (h == NULL)
+		return (NULL);
+	if (*h == NULL && idx != 0)
+		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 	temp = *h;
-	while (temp->next != NULL)
+	while (temp != NULL && count < idx)
 	{
 		temp = temp->next;
 		count++;
 	}
-	if (idx > count)
+	if (count < idx)
 		return (NULL);
-	if (idx == 1)
-		add_dnodeint(h, n);
-	else if (idx == count)
-		add_dnodeint_end(h, n);
-	else
-	{
-		temp = *h;
-		newnode = (dlistint_t *)malloc(sizeof(dlistint_t));
-		if (newnode == NULL)
-			return (NULL);
-		newnode->n = n;
-		while (i < idx)
-		{
-			temp = temp->next;
-			i++;
-		}
-		newnode->prev = temp->prev;
-		newnode->next = temp;
-		temp->prev->next =  newnode;
-		temp->prev = newnode;
-	}
+	if (temp == NULL)
+		return (add_dnodeint_end(h, n));
+	newnode = (dlistint_t *)malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+	newnode->prev = temp->prev;
+	newnode->next = temp;
+	temp->prev->next = newnode;
+	temp->prev = newnode;
+
 	return (newnode);
 }
