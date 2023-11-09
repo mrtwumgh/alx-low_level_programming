de09852f420ff6d4cff1f738f266e25dd4bf4abb
mrtwumgh <twumstephen2006@gmail.com>
2023-11-09 05:28:01 +0000

    3-hash_table_set.c fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,6 +36,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		free(newnode);
 		return (0);
+	}
 	newnode->next = ht->array[index];
 	ht->array[index] = newnode;
 
