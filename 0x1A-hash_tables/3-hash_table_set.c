f3debb8ce1b7754ef018ea5df51456fb8d318c6b
mrtwumgh <twumstephen2006@gmail.com>
2023-11-09 05:27:14 +0000

    3-hash_table_set.c fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,6 +14,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = 0;
 	hash_node_t *newnode = NULL;
 
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+	{
+		return (0);
+	}
 	index = key_index((const unsigned char *)key, ht->size);
 	newnode = (hash_node_t *)malloc(sizeof(hash_node_t));
 	if (newnode == NULL)
@@ -22,7 +26,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	newnode->key = strdup(key);
+	if (newnode->key == NULL)
+	{
+		free(newnode);
+		return (0);
+	}
 	newnode->value = strdup(value);
+	if (newnode->value == NULL)
+	{
+		free(newnode);
+		return (0);
 	newnode->next = ht->array[index];
 	ht->array[index] = newnode;
 
